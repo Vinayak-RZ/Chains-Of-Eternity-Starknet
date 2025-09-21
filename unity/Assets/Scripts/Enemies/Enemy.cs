7d9a9f2d38217a0eb78199bfd4bdb8a52c945ee1
LordRyuga <saketishaan123@gmail.com>
2025-09-21 06:14:07 +0530

    feat(player): adds new player assets and animations, and improves state machines

--- unity/Assets/Scripts/Enemies/Enemy.cs
+++ unity/Assets/Scripts/Enemies/Enemy.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Threading.Tasks;
 using Unity.IO.LowLevel.Unsafe;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -16,8 +17,19 @@ public class Enemy : MonoBehaviour
     public float attackSpeed = 1f; // Attacks per second
     public float AttackRange = 1.5f;
     public float AttackCooldown = 1f;
-    public float knockbackForce = 5f; // Force applied when knocked back
+    public float knockbackForce = 7f; // Force applied when knocked back
+    public float attackanimationtime = 1f; // Duration of stun effect
+    public UInt32 maxExpAward;
+    public UInt32 minExpAward;
+    private int expAward;
     public String Name = "Enemy";
+
+    private bool hasDied = false;
+    private bool fetchedExp = false;
+
+    private float timer;
+
+    private TaskCompletionSource<UInt32> expTCS;
 
     public NavMeshAgent agent;
 
@@ -49,17 +61,33 @@ public class Enemy : MonoBehaviour
     public FollowState FollowState { get; private set; }
     public AttackState AttackState { get; private set; }
     public StunState StunState { get; private set; }
+    public CooldownState CooldownState { get; private set; }
+    public SpriteRenderer spriteRenderer { get; private set; }
+
+    private SimpleRandomContract simpleRandomContract;
 
     public bool IsFacingRight = true;
 
     private void Awake()
-    {
+    {   
+        spriteRenderer = GetComponent<SpriteRenderer>();
         if (playerTransform == null)
             playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
 
         RB = transform.GetComponent<Rigidbody2D>();
     }
 
+    void OnEnable()
+    {
+        if (VRFResultRouter.Instance != null)
+            VRFResultRouter.Instance.OnVRFResult += OnVRFResult;
+    }
+
+    void OnDisable()
+    {
+        if (VRFResultRouter.Instance != null)
+            VRFResultRouter.Instance.OnVRFResult -= OnVRFResult;
+    }
     void Start()
     {
         RB.freezeRotation = true;
@@ -69,14 +97,18 @@ public class Enemy : MonoBehaviour
         agent.updateUpAxis = false; // Disable automatic up-axis adjustment if not needed
 
         StateMachine = new StateMachine<Enemy>();
+        simpleRandomContract = new SimpleRandomContract();
 
         IdleState = new IdleState(this, StateMachine);
         FreeRoamingState = new FreeRoamingState(this, StateMachine);
         FollowState = new FollowState(this, StateMachine);
         AttackState = new AttackState(this, StateMachine);
         StunState = new StunState(this, StateMachine);
+        CooldownState = new CooldownState(this, StateMachine, AttackCooldown);
         StateMachine.Initialize(IdleState);
 
+        expAward = (int) UnityEngine.Random.Range(minExpAward, maxExpAward);
+        StartCoroutine(enumerator());
     }
 
     private void Update()
@@ -110,13 +142,15 @@ public class Enemy : MonoBehaviour
             layerMask: playerLayer | obstacleLayer // Combine masks
         );
 
+        Debug.Log("Performed raycast for player detection");
+
         if (hit)
         {
             // If the hit is the player, return true; if it's an obstacle, return false.
             if (((1 << hit.collider.gameObject.layer) & playerLayer) != 0)
                 return true;
         }
-
+        Debug.Log("Player not detected within vision range or blocked by an obstacle.");
         return false;
     }
 
@@ -146,26 +180,39 @@ public class Enemy : MonoBehaviour
         }
         return targetPos;
     }
-    public void TakeDamage(int damage, Vector3 sourcePos, bool applyKnockback = true, bool applyStun = true, string damageType = "Physical")
+    public void TakeDamage(int damage, Vector3 sourcePos, bool applyKnockback = true, bool applyStun = true, bool flash = true, string damageType = "Physical")
     {
         //TODO : Handle different damage types (e.g., Physical, Magical)
         int effectiveDamage = Mathf.Max(0, damage);
 
         currentHealth = Mathf.Max(0, currentHealth - effectiveDamage);
-
-        Debug.Log(sourcePos);
-        Debug.Log(transform.position);
-        Debug.Log(transform.position - sourcePos);
+        animator.SetBool("isHit", true);
+        //Debug.Log(sourcePos);
+        //Debug.Log(transform.position);
+        //Debug.Log(transform.position - sourcePos);
         Vector2 knockbackDirection = ((Vector2)transform.position - (Vector2)sourcePos).normalized;
-        Debug.Log(knockbackDirection);
+        //Debug.Log(knockbackDirection);
         //knockbackDirection = new Vector2(knockbackDirection.x, -knockbackDirection.y);
-        Debug.Log(knockbackDirection);
+        //Debug.Log(knockbackDirection);
         if (applyKnockback)
             ApplyKnockback(knockbackDirection, applyStun, 5f);
-        if (currentHealth == 0)
+        if (flash)
+            StartCoroutine(FlashOnHit());
+
+        if (currentHealth == 0 && !hasDied)
             Die();
     }
-    public void ApplyKnockback(Vector2 direction, bool applyStun, float force= 0.1f,  float duration = 0.2f)
+    private IEnumerator FlashOnHit()
+    {
+        
+        if (spriteRenderer == null) yield break; // No sprite renderer to flash
+
+        Color originalColor = spriteRenderer.color;
+        spriteRenderer.color = Color.red*2; // Flash color
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = originalColor; // Reset to original color
+    }
+    public void ApplyKnockback(Vector2 direction, bool applyStun, float force = 0.1f, float duration = 0.2f)
     {
         StartCoroutine(KnockbackRoutine(direction, force, duration, applyStun));
     }
@@ -200,11 +247,14 @@ public class Enemy : MonoBehaviour
     {
         //Debug.Log("Flipping" + Name);
         IsFacingRight = !IsFacingRight;
-        RB.transform.Rotate(0f, 180f, 0f);
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
     
     public void Die()
     {
+        hasDied = true;
+        
+        animator.SetBool("isDead", true);
         Debug.Log(Name + " has died.");
         //TODO: Implement death logic, like playing a death animation, dropping loot, etc.
         onDeath();
@@ -218,5 +268,49 @@ public class Enemy : MonoBehaviour
     {
         // Override this method to implement custom death behavior
         Debug.Log(Name + " has been defeated!");
+    }
+
+    public void hitAnimComplete()
+    {
+        animator.SetBool("isHit", false);
+    }
+
+    void OnVRFResult(uint exp)
+    {
+        Debug.Log($"?? Awarding {exp} EXP to player");
+
+        expAward = (int)exp;
+    }
+
+    void HandleVRFResult(uint result)
+    {
+        string myAddress = Web3AuthManager.Instance.GetWalletAddress();
+
+        // Optional: if using `requestId` later, you can match here
+        // For now, just resolve the Task with the exp
+        if (expTCS != null && !expTCS.Task.IsCompleted)
+        {
+            expTCS.SetResult(result);
+        }
+    }
+
+    private IEnumerator enumerator()
+    {
+        yield return new WaitForSeconds(1f);
+
+        fetchExpFromContract();
+    }
+
+    private async void fetchExpFromContract()
+    {
+        expTCS = new TaskCompletionSource<UInt32>();
+        VRFResultRouter.Instance.OnVRFResult += HandleVRFResult;
+        Debug.Log("Requesting random experience award...");
+        string txnHash = await simpleRandomContract.RequestRandom(minExpAward, maxExpAward);
+        Debug.Log($"VRF request sent with transaction hash: {txnHash}");
+        Debug.Log("Waiting for VRF result...");
+        UInt32 exp = await expTCS.Task;
+        Debug.Log($"Received EXP: {exp} from VRF request {txnHash}");
+        ProgressManager.Instance.AddExperience((int)exp);
     }
 }
--- unity/Assets/Scripts/Enemies/EnemyState.cs
+++ unity/Assets/Scripts/Enemies/EnemyState.cs
@@ -1,3 +1,4 @@
+using NavMeshPlus.Extensions;
 using UnityEngine;
 
 public abstract class BaseEnemyState : State<Enemy>
@@ -9,14 +10,22 @@ public abstract class BaseEnemyState : State<Enemy>
     protected Rigidbody2D Rb => owner.RB;
     protected float MoveSpeed => owner.MoveSpeed;
 
-    protected void MoveTowards(Vector2 target)
+    protected void MoveTowards(Vector2 target, float moveSpeed)
     {
-        Vector2 direction = (target - (Vector2)owner.transform.position).normalized;
-        Rb.linearVelocity = direction * MoveSpeed;
+        if(owner.agent.isActiveAndEnabled)
+        {
+            owner.agent.speed = moveSpeed;
+            owner.agent.SetDestination(target);
+        }
     }
 
     protected void StopMoving()
     {
-        Rb.linearVelocity = Vector2.zero;
+        owner.agent.speed = 0;
+        if(owner.RB != null)
+        {
+            owner.RB.linearVelocity = Vector2.zero;
+        }
+        //owner.RB.linearVelocity = Vector2.zero;
     }
 }
