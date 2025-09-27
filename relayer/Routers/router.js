c80959c0f4b7d63196f4646363f6f7da5caf56a1
Dhanraj Kumar <dk7631333778@gmail.com>
2025-09-27 15:31:40 +0630

    feat(relayer): add marketplace event catchers

--- relayer/Routers/router.js
+++
@@ -1,9 +0,0 @@
-import {express} from 'express';
-import { heroCollection } from "../controllers/CollectionHero-test.js";
-import { mintHero } from "../controllers/HeroNFT.js";
-
-const router = express.Router();
-
-router.post("/mint-hero", mintHero);
-
-router.post("/hero-collection", heroCollection);
