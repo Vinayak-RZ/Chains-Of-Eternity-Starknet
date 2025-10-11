024957218ae288d813d574aee75e7675ce4da801
Vinayak-RZ <vinayakraizada@gmail.com>
2025-10-12 00:55:20 +0530

    feat(dojo): imports dojo and Newton soft package

--- unity/Assets/Scripts/Core/Managers/GameStates/InGameState.cs
+++ unity/Assets/Scripts/Core/Managers/GameStates/InGameState.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class InGameState : State<GameManager>
+public class InGameState : State<GameManagery>
 {
-    public InGameState(GameManager owner, StateMachine<GameManager> stateMachine)
+    public InGameState(GameManagery owner, StateMachine<GameManagery> stateMachine)
         : base(owner, stateMachine) { }
 
     public override void Enter()
